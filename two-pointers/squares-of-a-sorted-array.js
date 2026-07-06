f7228a884484a367da4a4d6424763b35e813957b
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2026-07-06 09:56:29 +0530

    docs: add complexity analysis and algorithmic explanation to sortedSquares implementation

--- two-pointers/squares-of-a-sorted-array.js
+++ two-pointers/squares-of-a-sorted-array.js
@@ -1,9 +1,10 @@
 // https://leetcode.com/problems/squares-of-a-sorted-array/
 
+//Using two pointers
+// TC: O(n) SC: O(n)
 var sortedSquares = function (nums) {
     let n = nums.length;
     let result = new Array().fill(0);
-
 
     let left = 0; right = n - 1;
     for (let i = n - 1; i >= 0; i--) {
